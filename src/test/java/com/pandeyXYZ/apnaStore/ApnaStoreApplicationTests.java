a8356e95aab07ada10507b83bb521f043271d92f
Ganesh Pandey <pandey.ganesh12@gmail.com>
2022-03-01 12:14:42 +0530

    Initial commit (Spring Initializer)
    Initial commit (Spring Initializer)

---
+++ src/test/java/com/pandeyXYZ/apnaStore/ApnaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandeyXYZ.apnaStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApnaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
